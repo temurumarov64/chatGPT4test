6e9b70e545b0f229a95403286ab1e124ecf2e3c9
Temur Umarov <umarovtemur25@gmail.com>
2023-11-04 18:50:16 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import bodyParser from "body-parser";
+import OpenAI from "openai";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/message", (req, res) => {
+  const message = req.body.message;
+  const response = openai.chat.completions.create({
+    model: "gpt-4",
+    messages: [
+      {
+        role: "user",
+        content: message,
+      },
+    ],
+  });
+
+  response
+    .then((result) => {
+      res.send(result.choices[0].message.content);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
