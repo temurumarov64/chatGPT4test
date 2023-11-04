5992c7cc91c525c2cd726da02f6be3e49e834ddf
Temur Umarov <umarovtemur25@gmail.com>
2023-11-04 18:56:29 +0500

    test1

--- index.js
+++ index.js
@@ -41,6 +41,6 @@ app.post("/message", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
